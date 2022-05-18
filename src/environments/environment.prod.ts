34c5c2d3f7eb6b62a422ad377a3a1ec8fab3928a
Adotac <otacool89@gmail.com>
2022-05-18 15:06:30 +0800

    added firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'airline-booking-system-c9d55',
+    appId: '1:405266574056:web:85782026d1641afc4c375d',
+    storageBucket: 'airline-booking-system-c9d55.appspot.com',
+    locationId: 'asia-southeast2',
+    apiKey: 'AIzaSyD2GaxrZDrcSlHYULb-kFQ7WQO73YNBvZ8',
+    authDomain: 'airline-booking-system-c9d55.firebaseapp.com',
+    messagingSenderId: '405266574056',
+    measurementId: 'G-S3YNTW9J45',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'airline-booking-system-c9d55',
+    appId: '1:405266574056:web:85782026d1641afc4c375d',
+    storageBucket: 'airline-booking-system-c9d55.appspot.com',
+    locationId: 'asia-southeast2',
+    apiKey: 'AIzaSyD2GaxrZDrcSlHYULb-kFQ7WQO73YNBvZ8',
+    authDomain: 'airline-booking-system-c9d55.firebaseapp.com',
+    messagingSenderId: '405266574056',
+    measurementId: 'G-S3YNTW9J45',
+  },
   production: false
 };
 

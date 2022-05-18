9cec12e206716a0255da9af39c6ec80e8b6ade7c
Adotac <otacool89@gmail.com>
2022-05-18 16:11:55 +0800

    added userpage and screen folder
    -ng generate component userpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { UserpageComponent } from './screens/user/userpage/userpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserpageComponent
   ],
   imports: [
     BrowserModule,

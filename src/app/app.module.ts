4af4d3a42a680148d2fd8540c055b50359ddf8f5
Martin <lapetajemartin@gmail.com>
2022-05-27 20:41:11 +0800

    refactored UI (partial)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { UserpageComponent } from './screens/userpage/userpage.component';
 import { AdminpageComponent } from './screens/adminpage/adminpage.component';
 import { AuthPageComponent } from './screens/auth-page/auth-page.component';
@@ -16,6 +16,13 @@ import { AdminFlightsComponent } from './screens/adminpage/admin-flights/admin-f
 import { AdminBookingsComponent } from './screens/adminpage/admin-bookings/admin-bookings.component';
 import { UserFlightsComponent } from './screens/userpage/user-flights/user-flights.component';
 import { UserBookingsComponent } from './screens/userpage/user-bookings/user-bookings.component';
+import { FormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import {
+  NgxMatDatetimePickerModule,
+  NgxMatNativeDateModule,
+  NgxMatTimepickerModule,
+} from '@angular-material-components/datetime-picker';
 
 @NgModule({
   declarations: [
@@ -28,16 +35,21 @@ import { UserBookingsComponent } from './screens/userpage/user-bookings/user-boo
     AdminFlightsComponent,
     AdminBookingsComponent,
     UserFlightsComponent,
-    UserBookingsComponent
+    UserBookingsComponent,
   ],
   imports: [
+    MatDatepickerModule,
+    FormsModule,
+    NgxMatNativeDateModule,
+    NgxMatDatetimePickerModule,
+    NgxMatTimepickerModule,
     BrowserModule,
     AppRoutingModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore())
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/screens/adminpage/admin-flights/admin-flights.component.ts
+++ src/app/screens/adminpage/admin-flights/admin-flights.component.ts
@@ -1,15 +1,80 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-admin-flights',
   templateUrl: './admin-flights.component.html',
-  styleUrls: ['./admin-flights.component.scss']
+  styleUrls: ['./admin-flights.component.scss'],
 })
 export class AdminFlightsComponent implements OnInit {
+  // constructor() {}
+  // @ViewChild('picker') picker: any;
 
-  constructor() { }
-
+  date!: Date;
   ngOnInit(): void {
+    this.date = new Date(2021, 9, 4, 5, 6, 7);
   }
+  //   (function () {
+  //     var dateTimeController = function ($scope:any, $rootScope :any) {
+  //         $scope.vm = {
+  //             message: "Bootstrap DateTimePicker Directive",
+  //             dateTime: {}
+  //         };
 
+  //         $scope.$watch('change', function(){
+  //             console.log($scope.vm.dateTime);
+  //         });
+
+  //         /*
+  //            $scope.$on('emit:dateTimePicker', function (e, value) {
+  //            $scope.vm.dateTime = value.dateTime;
+  //            console.log(value);
+  //            })
+  //         */
+  //     };
+  //     var dateTimePicker = function ($rootScope :any) {
+
+  //         return {
+  //             require: '?ngModel',
+  //             restrict: 'AE',
+  //             scope: {
+  //                 pick12HourFormat: '@',
+  //                 language: '@',
+  //                 useCurrent: '@',
+  //                 location: '@'
+  //             },
+  //             link: function (scope :any, elem:any, attrs:any) {
+  //                 elem.datetimepicker({
+  //                     pick12HourFormat: scope.pick12HourFormat,
+  //                     language: scope.language,
+  //                     useCurrent: scope.useCurrent
+  //                 })
+
+  //                 //Local event change
+  //                 elem.on('blur', function () {
+
+  //                     console.info('this', this);
+  //                     console.info('scope', scope);
+  //                     console.info('attrs', attrs);
+
+  //                     /*// returns moments.js format object
+  //                     scope.dateTime = new Date(elem.data("DateTimePicker").getDate().format());
+  //                     // Global change propagation
+
+  //                     $rootScope.$broadcast("emit:dateTimePicker", {
+  //                         location: scope.location,
+  //                         action: 'changed',
+  //                         dateTime: scope.dateTime,
+  //                         example: scope.useCurrent
+  //                     });
+  //                     scope.$apply();*/
+  //                 })
+  //             }
+  //         };
+  //     }
+
+  //     angular.module('dateTimeSandbox', []).run(['$rootScope', function ($rootScope:any) {
+  //     }]).controller('dateTimeController', ['$scope', '$http', dateTimeController
+  //     ]).directive('dateTimePicker', dateTimePicker);
+  // })();
 }

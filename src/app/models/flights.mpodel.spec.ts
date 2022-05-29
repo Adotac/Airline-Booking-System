b6de438acdf017ce7dd80acd0deaec246d2e558d
Adotac <otacool89@gmail.com>
2022-05-30 07:51:15 +0800

    added flight model

---
+++ src/app/models/flights.mpodel.spec.ts
@@ -0,0 +1,7 @@
+import { Flights } from './flights.mpodel';
+
+describe('Flights', () => {
+  it('should create an instance', () => {
+    expect(new Flights()).toBeTruthy();
+  });
+});
---
+++ src/app/models/flights.mpodel.ts
@@ -0,0 +1,7 @@
+export class Flights {
+    flight_code?: string;
+    depart_time?: any;
+    arrival_time?: any;
+    dest_name?: string;
+    origin_name?: string;
+}

0cc39fd4f388162f7ab13a4fb1112cd151bc1075
Adotac <otacool89@gmail.com>
2022-05-31 12:55:34 +0800

    added firebase service - working get all flights!

---
+++ src/app/services/abs-firebase.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { ABSFirebaseService } from './abs-firebase.service';
+
+describe('ABSFirebaseService', () => {
+  let service: ABSFirebaseService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ABSFirebaseService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});

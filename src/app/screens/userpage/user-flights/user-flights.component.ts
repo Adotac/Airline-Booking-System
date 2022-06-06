9da666f2d0dc55710c6567b636f1bfac437eb50f
Adotac <otacool89@gmail.com>
2022-06-06 22:10:51 +0800

    added book flight button and search flight panel

--- src/app/screens/userpage/user-flights/user-flights.component.ts
+++ src/app/screens/userpage/user-flights/user-flights.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Flights } from 'src/app/models/flights.mpodel';
 import { ABSFirebaseService } from 'src/app/services/abs-firebase.service';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
-import { Observable, pipe } from 'rxjs';
 import { filter, find, map, tap } from 'rxjs/operators';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -16,6 +15,8 @@ export class UserFlightsComponent implements OnInit {
   selectedFlight?: Flights;
   current_index = -1;
 
+  errorFormInput = '';
+
   constructor(private ABS_service: ABSFirebaseService) { }
   
   date!: Date;
@@ -23,6 +24,15 @@ export class UserFlightsComponent implements OnInit {
     this.date = new Date(2021, 9, 4, 5, 6, 7);
     this.retrieveFlights()
   }
+
+  flightForm: FormGroup = new FormGroup({
+    origin: new FormControl('', Validators.required),
+    destination: new FormControl('', Validators.required),
+    arivalDate: new FormControl('', Validators.required),
+    arivalTime: new FormControl('', Validators.required),
+    departureDate: new FormControl('', Validators.required),
+    departureTime: new FormControl('', Validators.required),
+  });
 
   retrieveFlights(){
     console.log("retrieve flights!!")
@@ -35,6 +45,44 @@ export class UserFlightsComponent implements OnInit {
     ).subscribe(data=>{
       this.flights = data;
     })
+  }
+
+  searchFlights(){
+    if (
+      !((this.flightForm.value.origin && this.flightForm.value.origin.trim()) 
+        || (this.flightForm.value.destination &&  this.flightForm.value.destination.trim()) )
+    ) {
+      if (
+        this.flightForm.value.departureDate ||this.flightForm.value.arivalDate 
+        ||this.flightForm.value.departureTime ||this.flightForm.value.arivalTime
+      )
+      {
+        if (
+          !(
+            this.ABS_service.isGoodDate(this.flightForm.value.departureDate) ||
+            this.ABS_service.isGoodDate(this.flightForm.value.arivalDate) ||
+            this.ABS_service.isGoodTime(this.flightForm.value.departureTime) ||
+            this.ABS_service.isGoodTime(this.flightForm.value.arivalTime)
+          )
+        ) {
+          this.errorFormInput = 'date or/and time is not valid';
+          return;
+        }
+        else{
+          this.errorFormInput = '';
+          return;
+        }
+      }
+
+
+      this.errorFormInput = 'empty fields';
+    }
+    else{
+      this.errorFormInput = '';
+    }
+
+    
+
   }
 
 }
